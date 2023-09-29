0e94adfeb7066acaebcffd4f26d12cb12e40b6a8
denver123-creator <59923346+denver123-creator@users.noreply.github.com>
2023-09-29 20:17:08 +0800

    Add files via upload

---
+++ assets/js/navbar.js
@@ -0,0 +1,39 @@
+function showMessagePopup() {
+    const wrapper = document.getElementById('wrapper'); 
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+
+    let bottomNav = `<nav id="nav">
+                    <a href="#" class="icon solid fa-home" id="a"><span>Home</span></a>
+                    <a href="#work" class="icon solid fa-folder" id="b"><span>Work</span></a>
+                    <a href="#product" class="icon solid fa-paint-roller"><span>Product</span></a>
+                    <a href="#contact" class="icon solid fa-envelope"  id="c"><span>Contact</span></a>
+                    <a href="#ai" class="icon solid fa-image"><span>AI Image Gen</span></a>
+                    <a href="https://www.facebook.com/alfredo.publico" class="icon brands fa-facebook" id="d"><span>Facebook</span></a>
+                    </nav>`;		
+    popup.innerHTML = bottomNav;
+    wrapper.appendChild(popup);
+    }
+
+    showMessagePopup()
+
+
+    window.addEventListener("scroll", function() {
+        const navTwo = document.querySelector('.popup');
+        const mainElement = document.getElementById('main');
+        const homeElement = document.getElementById('home'); 
+        const distanceFromTop = mainElement.getBoundingClientRect().top;
+
+
+        const triggerScrollPosition = 120; 
+
+        const homeHeight = homeElement.getBoundingClientRect().height;
+        const isAtTop = window.scrollY === triggerScrollPosition;
+
+        if (isAtTop || distanceFromTop <= homeHeight - triggerScrollPosition ) {
+    
+            navTwo.style.display = "block";
+        }else{
+            navTwo.style.display = "none";
+        }
+        });
