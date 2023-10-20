fc3bdf981145bfa098eaec4375975fab68da9aa5
denver123-creator <59923346+denver123-creator@users.noreply.github.com>
2023-10-19 19:53:09 -0700

    Add files via upload

---
+++ assets/profiler/baryabols.js
@@ -0,0 +1,83 @@
+const user1 = [{
+    name: "Alfredo Publico",
+    profession: "<span style=\"font-style:italic;\">Masterful Brushstrokes - </span>Discover the Artistry <br> of Canvas Creations.",
+    profilepic: "users/user1/profpic/pic01.jpg",
+    workpics: []
+}];
+
+const userWorks = [{
+  workpics: [
+      "users/user1/workpic/pic01.webp",
+      "users/user1/workpic/pic22.webp",
+      "users/user1/workpic/pic02.webp",
+      "users/user1/workpic/pic04.webp",
+      "users/user1/workpic/pic07.webp",
+      "users/user1/workpic/pic18.webp",
+      "users/user1/workpic/pic05.webp",
+      "users/user1/workpic/pic06.webp",
+      "users/user1/workpic/pic21.webp",
+      "users/user1/workpic/pic08.webp",
+      "users/user1/workpic/pic09.webp",
+      "users/user1/workpic/pic10.webp",
+      "users/user1/workpic/pic11.webp",
+      "users/user1/workpic/pic12.webp",
+      "users/user1/workpic/pic13.webp",
+      "users/user1/workpic/pic14.webp",
+      "users/user1/workpic/pic15.webp",
+      "users/user1/workpic/pic16.webp",
+      "users/user1/workpic/pic17.webp",
+      "users/user1/workpic/pic19.webp",
+      "users/user1/workpic/pic20.webp",
+      "users/user1/workpic/pic03.webp"
+  ]
+}];
+
+const userProducts = [{
+            title: "Fruitcake (2020)",
+            description: `There's a fruitcake for everybody
+            There's a fruitcake for everyone
+            There are b-sides to every story
+            If you decide to have some fun Take a bite
+            It's alright.`,
+            image: "users/user1/product-image/5.webp",
+            video: "users/user1/product-video/video3.webm",
+            price: 999.99
+        }, {
+            title: "Somewhere over the rainbow (2019)",
+            description: `Somewhere over the rainbow
+            Skies are blue
+            And the dreams that you dare to dream
+            Really do come true.`,
+            image: "users/user1/product-image/4.webp",
+            video: "users/user1/product-video/video3.webm",
+            price: 999.99
+        }, {
+            title: "Bed of Roses",
+            description: `I want to lay you down in a bed of roses
+            For tonight I'll sleep on a bed of nails
+            Oh I want to be just as close as the Holy Ghost is
+            And lay you down on a bed of roses.`,
+            image: "users/user1/product-image/1.webp",
+            video: "users/user1/product-video/video1.mp4",
+            price: 999.99
+        }, {
+            title: "House of the Rising Sun",
+            description: `There is a house way down in New Orleans
+            They call the Rising Sun
+            And it's been the ruin of many a poor boy
+            And God I know I'm one.`,
+            image: "users/user1/product-image/2.webp",
+            video: "users/user1/product-video/video2.webm",
+            price: 999.99
+        }, {
+            title: "Hotel California",
+            description: `Welcome to the Hotel California
+            Such a lovely place 
+            Plenty of room at the Hotel California
+            Any time of year
+            You can find it here.`,
+            image: "users/user1/product-image/3.webp",
+            video: "users/user1/product-video/video3.webm",
+            price: 999.99
+          }
+];
